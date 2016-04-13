1df17ffaa43db6dfb1d3bd85331fb5f51b9f067f
athos <athos@vialink.com.br>
2016-04-13 09:18:35 -0300

    Removing input files and wrong subimissions.

--- seletiva_ufpe/j.cpp
+++ seletiva_ufpe/j.cpp
@@ -1,33 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 15;
-int t, T, n, n0, c, m[N][N], id[N], s[N], ans, aux[N][N];
-
-bool cp(int i, int j) {return s[i] < s[j];}
+const int N = 15, M = 1<<N, INF = 1e9+1;
+int t, T, n, c[N], dp[M], k, x, m[N][M];
 
 int main() {
     scanf("%d", &T);
     while (t++ < T) {
-        ans = 0;
-        scanf("%d", &n0);
-        for(int i=0; i<n0; ++i) scanf("%d", &c), ans -= c;
-        for(int i=0; i<n0; ++i) for(int j=0; j<n0; ++j) scanf("%d", &m[i][j]);
+        scanf("%d", &n);
+        k = 1<<n;
 
-        for(n=n0; n>1; --n) {
-            memset(s, 0, sizeof s);
-            for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) s[i] += m[i][j] - m[j][i];
+        for(int i=1; i<k; ++i) dp[i] = -INF;
 
-            for(int i=0; i<n; ++i) id[i] = i;
-            sort(id, id+n, cp);
+        for(int i=0; i<n; ++i) scanf("%d", c+i);
+        for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) scanf("%d", &m[i][1<<j]);
+        for(int i=0; i<n; ++i) m[i][1<<i] = 0;
 
-            for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) aux[i][j] = m[i][j];
-            for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) m[i][j] = aux[id[i]][id[j]];
-        }
 
-        for(int i=0; i<n0; ++i) for(int j=0; j<i; ++j) ans += m[i][j];
+        for(int i=0; i<n; ++i) for(int j=1; j<k; ++j) m[i][j] = m[i][j-(j&-j)] + m[i][j&-j];
 
-        printf("Caso #%d: %d\n", t, ans);
+        for(int i=0; i<k; ++i)
+            for(int j=0; (1<<j) <= i; ++j)
+                if (i & (1<<j)) dp[i] = max(dp[i], dp[i^(1<<j)] - c[j] + m[j][i]);
+
+        printf("Caso #%d: %d\n", t, dp[k-1]);
     }
     return 0;
 }
